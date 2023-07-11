2f33e087813a1dd6420a38a3ce2952077d295f00
Schaban Bochi <schaban.bochi@dtele.de>
2023-07-11 15:37:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pub Manager
+
+Pub Manager is a Visual Studio Code extension that helps you manage packages in your Dart and Flutter projects. With this extension, you can view package information, check for updates, and easily update your `pubspec.yaml` file.
+
+## Features
+
+- Display a list of packages from the `pubspec.yaml` file in a webview panel.
+- View package details such as the current version, latest version, and published date.
+- Check for available updates for each package and perform updates.
+- Automatically update the `pubspec.yaml` file and run `flutter pub get` to apply package updates.
+
+## Installation
+
+1. Launch Visual Studio Code.
+2. Open the Extensions view by clicking on the square icon in the sidebar or by pressing `Ctrl+Shift+X`.
+3. Search for "Pub Manager" and click on the "Install" button.
+4. Once the installation is complete, click on the "Reload" button to activate the extension.
+
+## Usage
+
+1. Open a Dart or Flutter project that contains a `pubspec.yaml` file and open it in editor.
+2. In the VS Code activity bar, click on the Pub Manager icon to open the package list.
+3. The package list will be displayed in a webview panel, showing the package name, current version, latest version, published date, and an update button.
+4. Click on the update button for a package to update its version in the `pubspec.yaml` file.
+5. After the update, the `pubspec.yaml` file will be saved, and `flutter pub get` will be automatically run to apply the changes.
+
+## Known Issues
+
+- There are no known issues at the moment. If you encounter any problems or have suggestions for improvements, please [open an issue](https://github.com/SchabanBo/pub_manager/issues).
+
+---
+
+Happy coding!
---
+++ src/extension.ts
@@ -0,0 +1,246 @@
+import * as vscode from 'vscode';
+import * as yaml from 'yaml';
+import * as semver from 'semver';
+import axios from 'axios';
+import * as childProcess from 'child_process';
+import * as fs from 'fs';
+import { log } from 'console';
+
+interface PackageData {
+	latestVersion: string;
+	publishedDate: string;
+}
+
+let pubspecContent = '';
+
+function getPubspecContent(): string {
+	const activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor && activeEditor.document.languageId === 'yaml' && activeEditor.document.fileName.endsWith('pubspec.yaml')) {
+		return activeEditor.document.getText();
+	}
+	return '';
+}
+
+function checkActiveEditor() {
+	const content = getPubspecContent();
+	const isPubspecFile = content.length > 0;
+	if (isPubspecFile) {
+		pubspecContent = content;
+	}
+	vscode.commands.executeCommand('setContext', 'isPubspecFile', isPubspecFile);
+}
+
+async function fetchPackageData(packageName: string): Promise<PackageData> {
+	try {
+		const response = await axios.get(`https://pub.dev/api/packages/${packageName}`);
+		const latestVersion = response.data.latest.version;
+		const versionInfo = await axios.get(`https://pub.dev/api/packages/${packageName}/versions/${latestVersion}`);
+		const publishedDate = new Date(versionInfo.data.published).toLocaleDateString();
+		return {
+			latestVersion,
+			publishedDate,
+		};
+	} catch (error) {
+		console.error(`Error fetching package data for ${packageName}:`, error);
+		return {
+			latestVersion: '',
+			publishedDate: '',
+		};
+	}
+}
+
+async function handleUpdateClick(packageName: string, newVersion: string) {
+	console.log('Update clicked for package:', packageName);
+
+	const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+	if (!workspaceFolder) {
+		vscode.window.showErrorMessage('Please open a pubspec.yaml file in a workspace before performing the update.');
+		return;
+	}
+
+	const pubspecPath = vscode.Uri.joinPath(workspaceFolder.uri, 'pubspec.yaml');
+	try {
+		const fileContent = fs.readFileSync(pubspecPath.fsPath, 'utf8');
+		const updatedContent = modifyPubspecContent(fileContent, packageName, newVersion);
+		pubspecContent = updatedContent;
+		fs.writeFileSync(pubspecPath.fsPath, updatedContent, 'utf8');
+		await runPubGetCommand(workspaceFolder.uri.fsPath);
+		vscode.window.showInformationMessage(`Updated package: ${packageName} to version ${newVersion}`);
+	} catch (error) {
+		console.error(`Error updating package: ${packageName}`, error);
+	}
+}
+
+function modifyPubspecContent(pubspecContent: string, packageName: string, newVersion: string): string {
+	const lines = pubspecContent.split('\n');
+	const updatedLines = lines.map((line) => {
+		if (line.includes(packageName)) {
+			const currentVersion = line.split(':')[1].trim();
+			return line.replace(currentVersion, newVersion);
+		}
+		return line;
+	});
+	return updatedLines.join('\n');
+}
+
+async function runPubGetCommand(cwd: string): Promise<void> {
+	try {
+		await runCommand('flutter pub get', { cwd });
+		console.log('flutter pub get executed successfully');
+	} catch (error) {
+		console.error('Error executing flutter pub get:', error);
+	}
+}
+
+function runCommand(command: string, options: childProcess.ExecOptions): Promise<string> {
+	return new Promise((resolve, reject) => {
+		childProcess.exec(command, options, (error, stdout, stderr) => {
+			if (error) {
+				reject(error);
+			} else if (stderr) {
+				reject(new Error(stderr));
+			} else {
+				resolve(stdout.trim());
+			}
+		});
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Pub manager is now active');
+
+	let activeEditor = vscode.window.activeTextEditor;
+	checkActiveEditor();
+
+	vscode.window.onDidChangeActiveTextEditor((editor) => {
+		activeEditor = editor;
+		checkActiveEditor();
+	});
+
+	async function reloadPanelContent(panel: vscode.WebviewPanel) {
+		if (pubspecContent.length === 0) {
+			panel.webview.html = '<p>No packages found.</p>';
+			return;
+		}
+
+		try {
+			const pubspec = yaml.parse(pubspecContent);
+			if (pubspec && pubspec.dependencies) {
+				const dependencies = Object.keys(pubspec.dependencies).filter((dependency) => {
+					const name = pubspec.dependencies[dependency];
+					if (typeof name !== 'string') {
+						return false;
+					}
+					const currentVersion = pubspec.dependencies[dependency].toString().replace(/[\^~]/, '').toString();
+					return semver.valid(currentVersion) !== null;
+				});
+
+				const packageDataList = await Promise.all(dependencies.map(async (dependency) => {
+					const currentVersion = pubspec.dependencies[dependency].toString().replace(/[\^~]/, '').toString();
+					const packageData = await fetchPackageData(dependency);
+					const publishedDate = packageData.publishedDate;
+					const canBeUpdated = semver.gt(packageData.latestVersion, currentVersion);
+					const dependencyName = `<a href="https://pub.dev/packages/${dependency}" target="_blank">${dependency}</a>`;
+					const latestVersion = `<a href="https://pub.dev/packages/${dependency}/changelog" target="_blank">${packageData.latestVersion}</a>`;
+					const updateButton = canBeUpdated
+						? `<a><img src="${panel.webview.asWebviewUri(vscode.Uri.file(context.asAbsolutePath('/assets/icons/upgrade.svg')))}" alt="Upgrade" class="icon" onclick="handleUpdateClick('${dependency}', '${packageData.latestVersion}')"></a>`
+						: `<img src="${panel.webview.asWebviewUri(vscode.Uri.file(context.asAbsolutePath('/assets/icons/check.svg')))}" alt="latest version" class="icon">`;
+					return {
+						dependencyName,
+						currentVersion,
+						latestVersion,
+						publishedDate,
+						updateButton,
+					};
+				}));
+
+				const tableHtml = `<table class="package-table">
+          <tr>
+            <th></th>
+            <th>Package</th>
+            <th>Version</th>
+            <th>Latest Version</th>
+            <th>Published Date</th>
+          </tr>
+          ${packageDataList
+						.map(
+							(packageData) => `<tr>
+              <td>${packageData.updateButton}</td>
+              <td>${packageData.dependencyName}</td>
+              <td>${packageData.currentVersion}</td>
+              <td>${packageData.latestVersion}</td>
+              <td>${packageData.publishedDate}</td>
+              </tr>`
+						)
+						.join('')}
+        </table>`;
+
+				panel.webview.html = `
+          <style>
+            body {
+              font-size: 18px;
+            }
+            a {
+              text-decoration: none;
+            }
+            .icon {
+              width: 24px;
+              height: 24px;
+              cursor: pointer;
+            }
+            .package-table {
+              border-collapse: collapse;
+              width: 100%;
+            }
+            .package-table th,
+            .package-table td {
+              padding: 8px;
+              border-bottom: 1px solid var(--vscode-settings-textInputForeground);
+            }
+            .package-table th {
+              text-align: left;
+            }
+          </style>
+          ${tableHtml}
+          <script>
+            const vscode = acquireVsCodeApi();
+            function handleUpdateClick(package, version) {
+              vscode.postMessage({ command: 'updatePackage', package, version });
+            }
+          </script>
+        `;
+				return;
+			}
+		} catch (error) {
+			console.error('Error parsing pubspec.yaml:', error);
+		}
+	}
+
+	let disposable = vscode.commands.registerCommand('extension.showPackagesPanel', () => {
+		const panel = vscode.window.createWebviewPanel('packageListPanel', 'Package List', vscode.ViewColumn.Two);
+		panel.iconPath = vscode.Uri.file(context.asAbsolutePath('/assets/icons//list-dark.svg'));
+		reloadPanelContent(panel);
+
+		panel.webview.options = {
+			enableScripts: true,
+		};
+
+		panel.webview.onDidReceiveMessage((message) => {
+			if (message.command === 'updatePackage') {
+				const packageName = message.package;
+				const version = message.version;
+				handleUpdateClick(packageName, version).then(() => {
+					reloadPanelContent(panel);
+				});
+			}
+		});
+
+		vscode.window.onDidChangeActiveTextEditor(() => {
+			checkActiveEditor();
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
